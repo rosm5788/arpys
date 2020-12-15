2117f2d227e31406e701e01f346c9e10d1ba29b0
Dushyant <duna1846@colorado.edu>
2020-12-14 18:44:39 -0600

    Added metadata for diamond loader
    Also added convert to count rate utility in loader itself

--- arpys/loaders/diamond.py
+++ arpys/loaders/diamond.py
@@ -1,7 +1,6 @@
 from nexusformat.nexus import *
 import numpy as np
 import xarray as xr
-import arpys
 
 
 def load_diamond_consolidated(filename):
@@ -23,7 +22,8 @@ def load_diamond_consolidated(filename):
         # append perp label to beginning of axis_labels if the file is a fermi map
         axis_labels.insert(0, 'perp')
 
-    return xr.DataArray(vals, dims=axis_labels, coords=coords)
+    metadata = read_metadata(f)
+    return xr.DataArray(vals, dims=axis_labels, coords=coords, attrs=metadata)
 
 
 def load_diamond_fermi(filename):
@@ -54,3 +54,67 @@ def load_diamond_single(filename):
     vals = np.copy(counts[0, :, :])
 
     return xr.DataArray(vals, dims=axis_labels, coords=coords)
+
+
+def read_metadata(nxobject):
+
+    metadata = {}
+    # Read manipulator positions first
+    x = nxobject.entry1.instrument.manipulator.sax.nxvalue
+    y = nxobject.entry1.instrument.manipulator.say.nxvalue
+    z = nxobject.entry1.instrument.manipulator.saz.nxvalue
+    tilt = nxobject.entry1.instrument.manipulator.satilt.nxvalue
+    az = nxobject.entry1.instrument.manipulator.saazimuth.nxvalue
+    polar = nxobject.entry1.instrument.manipulator.sapolar.nxvalue
+
+    # Read Temperatures
+    cryostat = nxobject.entry1.sample.cryostat_temperature.nxvalue
+    shield = nxobject.entry1.sample.shield_temperature.nxvalue
+    sample = nxobject.entry1.sample.temperature.nxvalue
+
+    # monochromator/beam properties
+    bl_energy = nxobject.entry1.instrument.monochromator.energy.nxvalue
+    exit_slit_vert = nxobject.entry1.instrument.monochromator.exit_slit_size.nxvalue
+    exit_slit_horiz = nxobject.entry1.instrument.monochromator.exit_slit_size_horizontal.nxvalue
+    entrance_slit_vert = nxobject.entry1.instrument.monochromator.s2_vertical_slit_size.nxvalue
+    entrance_slit_horiz = nxobject.entry1.instrument.monochromator.s2_horizontal_slit_size.nxvalue
+    polarization = nxobject.entry1.instrument.insertion_device.beam.final_polarisation_label.nxvalue
+
+    # date and time properties
+    datetime = nxobject.entry1.end_time.nxvalue
+
+    # exposure settings for conversion to count rate (probably not valid for fixed mode scans, use with caution)
+    acq_mode = nxobject.entry1.instrument.analyser.acquisition_mode.nxvalue
+    energy_region_size = nxobject.entry1.instrument.analyser.cps_region_size.nxvalue[1]
+    dwell_time = nxobject.entry1.instrument.analyser.time_for_frames.nxvalue
+    sweeps = nxobject.entry1.instrument.analyser.number_of_iterations.nxvalue
+
+    metadata['x'] = x
+    metadata['y'] = y
+    metadata['z'] = z
+    metadata['tilt'] = tilt
+    metadata['az'] = az
+    metadata['polar'] = polar
+    metadata['cryostat T'] = cryostat
+    metadata['shield T'] = shield
+    metadata['sample T'] = sample
+    metadata['BL Energy'] = bl_energy
+    metadata['Polarization'] = polarization
+    metadata['exit slit vert'] = exit_slit_vert
+    metadata['exit slit horiz'] = exit_slit_horiz
+    metadata['entrance slit vert'] = entrance_slit_vert
+    metadata['entrance slit horiz'] = entrance_slit_horiz
+    metadata['time'] = datetime
+    metadata['Acquisition Mode'] = acq_mode
+    metadata['Energy Region Size'] = energy_region_size
+    metadata['Dwell Time'] = dwell_time
+    metadata['Sweeps'] = sweeps
+
+    return metadata
+
+
+# Convert Diamond spectra counts (2d spectra only) to count rate
+def convert_to_countrate(dataxarray):
+    total_time = dataxarray.attrs['Dwell Time'] * dataxarray.attrs['Energy Region Size'] * dataxarray.attrs['Sweeps']
+    count_rate_xarray = dataxarray / total_time
+    return count_rate_xarray
