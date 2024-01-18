6628812b6b45dfeff6998e770fa04644b104c303
Dushyant <duna1846@colorado.edu>
2024-01-18 16:33:46 -0600

    Hisor laser arpes loaders init

--- arpys/loaders/__init__.py
+++ arpys/loaders/__init__.py
@@ -7,5 +7,6 @@ from . import maestro
 from . import sis_ultra
 from . import diamond
 from . import BL10
+from . import hisor_laser
 
-__all__ = ['ssrl', 'dessauspecs', 'utility', 'merlin', 'maestro', 'sis_ultra', 'diamond', 'BL10', 'sls']
+__all__ = ['ssrl', 'dessauspecs', 'utility', 'merlin', 'maestro', 'sis_ultra', 'diamond', 'BL10', 'sls','hisor_laser']
