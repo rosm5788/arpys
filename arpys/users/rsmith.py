39d5acee7768e03128c9b6694bf1536970a49e1e
ROBERT-LAPTOP <robert.smithjr@colorado.edu>
2025-02-26 11:56:41 -0700

    Added stack_lines for MDCs (EDC support later) and algn_hvscan to correct monochromator drift

--- arpys/users/rsmith.py
+++ arpys/users/rsmith.py
@@ -1,6 +1,7 @@
 # Robert's functions
 import xarray as xr
 import numpy as np
+import matplotlib.pyplot as plt
 
 # used to normalize each perp. cut of a polar map not to be used for quantitative analysis, 
 # but is useful to more easily visualise FS shape and symmetry. Note: this uses an average
@@ -39,6 +40,19 @@ def normalize_3D(map):
         perp_normed = perp_normed.assign_coords({'perp':map['perp']})
     return perp_normed
 
+# pass a k-space spectra converted using a regular meshgrid. then, tweak peak_scale and nrg_spacing
+# so that the stacked lines dont peak too high and have an appropriate number of lines
+def stack_lines(spectra,nrg_spacing = 0.02,peak_scale=0.2,fig=None,ax=None,**kwargs):
+    if fig == None and ax == None:
+        fig, ax = plt.subplots(1,1)
+    max_nrg = np.max(spectra.coords['binding'].values)
+    min_nrg = np.min(spectra.coords['binding'].values)
+    nrg_range = np.linspace(min_nrg,max_nrg,int((max_nrg-min_nrg)/nrg_spacing),endpoint=True)
+    for nrg in nrg_range:
+        line = spectra.sel(binding=slice(nrg-nrg_spacing,nrg+nrg_spacing)).sum('binding')
+        line = ((line-np.min(line))/(np.max(line))*peak_scale) + nrg
+        ax.plot(spectra.coords['kx'].values,line,**kwargs)
+    return fig,ax
 
 # 2nd derivative functions: BOTH require exactly 2D datasets 3D seems unnecessary and
 # computationally expensive.
@@ -80,3 +94,32 @@ def cv2d(data, bwx=5, bwy=5, c1=0.001, c2=0.001, w=1):
     curvature = xr.DataArray(cv2d, dims=data.dims,
                             coords=data.coords, attrs=data.attrs)
     return curvature
+
+#aligns a photon_energy scan so that Ef = 0 binding, to correct
+#monochromator drift or similar effects
+def align_hvscan(hvscan,inclusion_zone=0.02):
+    hv_cuts = []
+    last_ef = hvscan.rename({'binding':'energy'}).sel(photon_energy=0,method='nearest').sel(energy=slice(-1,0.2)).sum('slit').arpes.guess_ef()
+    # fit each cut and align ef to 0 binding
+    for hv in hvscan['photon_energy']:
+        hv_cut = hvscan.sel({'photon_energy': hv}, method='nearest')
+        print(np.min(hv_cut.coords['binding'].values),np.max(hv_cut.coords['binding']).values)
+        nrg_max = np.min([np.max(hv_cut.coords['binding'].values)*inclusion_zone+last_ef,np.max(hv_cut.coords['binding'].values)])
+        nrg_min = np.max([np.min(hv_cut.coords['binding'].values)*inclusion_zone+last_ef,np.min(hv_cut.coords['binding'].values)])
+
+        edc = hv_cut.sel({'binding':slice(nrg_min,nrg_max)}).sel({'slit':slice(-15,15)}).sum('slit')
+        #edc.plot(ax=ax[1])
+        print("looking for eF in the range ",np.min(hv_cut.coords['binding'].values)*inclusion_zone+last_ef,np.max(hv_cut.coords['binding'].values)*inclusion_zone+last_ef)
+        last_ef = edc.rename({'binding':'energy'}).arpes.guess_ef()
+        print("ef found at ", last_ef)
+        new_be = hv_cut['binding'] - last_ef
+        hv_cuts.append(hv_cut.assign_coords({'binding': new_be}))
+
+    hv_cuts_interped = []
+    hv_cuts_interped.append(hv_cuts[0])
+    for scan_no in np.arange(1,len(hv_cuts)):
+        hv_cuts_interped.append(hv_cuts[scan_no].interp_like(hv_cuts[0]))
+
+    hv_cuts_aligned = xr.concat(hv_cuts_interped, 'photon_energy')
+    hv_cuts_aligned = hv_cuts_aligned.assign_coords({'photon_energy': hvscan['photon_energy']})
+    return hv_cuts_aligned
